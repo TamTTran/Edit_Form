7ecfdd7bf9074b6a1dcc40f9f2c60f55afb5f76a
TranThanhTam <tranthanhtam15vlh2@gmail.com>
2024-07-24 08:14:00 +0700

    first commit

---
+++ Model/Farm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Edit_Info.Model
+{
+    public class Farm
+    {
+        public string LocationID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Name_EINV { get; set; }
+
+        public string Address_EINV { get; set; }
+
+        public string Tax { get; set; }
+
+    }
+}
---
+++ ViewMoel/FarmVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edit_Info.ViewMoel
+{
+    public class FarmVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+
+    }
+}

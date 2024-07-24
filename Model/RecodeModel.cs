6e24e51b7a52be28dd844d43b28212f72ed51ccc
TranThanhTam <tranthanhtam15vlh2@gmail.com>
2024-07-24 15:16:35 +0700

    fix

---
+++ Model/RecodeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edit_Info.Model
+{
+    public class RecodeModel
+    {
+        public bool IsCheck { get; set; }
+
+        public string LocationID { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string LocationAddress { get; set; }
+
+        public string NameEINV { get; set; }
+
+        public string AddressEINV { get; set; }
+
+        public string Tax { get; set; }
+
+    }
+}
